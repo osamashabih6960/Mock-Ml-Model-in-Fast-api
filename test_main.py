cf827ef91df58ace70f957e7cb16ad905d9205ed
shabihosama@gmail.com <shab>
2025-08-22 20:40:44 +0530

    Mock ML DEMO

---
+++ test_main.py
@@ -0,0 +1,23 @@
+from fastapi.testclient import TestClient
+from unittest.mock import patch
+from main import app
+import numpy as np
+
+client = TestClient(app)
+
+
+def test_predict_with_mock():
+    with patch('model.model.predict') as mock_predict:
+        mock_predict.return_value = [99]
+        response = client.post(
+            '/predict',
+            json={
+                'SepalLengthCm': 5.5,
+                'SepalWidthCm': 2.1,
+                'PetalLengthCm': 4.3,
+                'PetalWidthCm': 1.25
+            }
+        )
+        assert response.status_code == 200
+        assert response.json() == {'prediction': 99}
+        # mock_predict.assert_called_once([5.5,2.1,4.3,1.25])
